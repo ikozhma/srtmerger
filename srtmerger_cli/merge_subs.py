d4a484ac79036fcbb605a1994587570941628a43
Ilia Kozhma <ikozhma_dev@proton.me>
2025-04-28 02:46:02 +0300

    Added option "top". Subs by default are merged and shown in the bottom.

--- srtmerger_cli/merge_subs.py
+++ srtmerger_cli/merge_subs.py
@@ -16,13 +16,14 @@ OUTPUT_PATH_TYPE = click.Path(exists=False, path_type=Path)
 @click.argument("lower_sub", type=SUB_ARG_TYPE)  # , cls=Path)
 @click.option("--upper_yellow", is_flag=True, help="Make upper sub yellow")
 @click.option("--lower_yellow", is_flag=True, help="Make lower sub yellow")
+@click.option("--top", is_flag=True,help="Place upper subs on top")
 @click.option(
     "--output",
     type=OUTPUT_PATH_TYPE,
     help="Path of the output file or folder. Default= folder of the upper_sub / '{upper_sub.stem}_{lower_sub.stem}_merged.srt'",
 )
 def merge_subs(
-    upper_sub: Path, lower_sub: Path, *, upper_yellow: bool = False, lower_yellow: bool = False, output: Optional[Path] = None
+    upper_sub: Path, lower_sub: Path, *, upper_yellow: bool = False, lower_yellow: bool = False, output: Optional[Path] = None, top : bool = False
 ) -> None:
     if {sub.suffix for sub in (upper_sub, lower_sub)} != {".srt"}:
         raise NotImplementedError("Script supports only .srt subs")
@@ -36,7 +37,7 @@ def merge_subs(
     else:
         m = Merger(output_name=merged_sub_name)
     logger.info("Reading upper_sub")
-    m.add(str(upper_sub), color=Colors.YELLOW if upper_yellow else Colors.WHITE, top=True)
+    m.add(str(upper_sub), color=Colors.YELLOW if upper_yellow else Colors.WHITE, top=top)
     logger.info("Reading lower_sub")
     m.add(str(lower_sub), color=Colors.YELLOW if lower_yellow else Colors.WHITE)
     # m.add('./test_srt/fa.srt', color="yellow", codec="cp1256", top=True)
